2140f50767ae0e09a8ed22a1a49eb468b6ddc860
wduntak <wduntak@gmail.com>
2016-04-17 13:26:24 -0400

    Component Lifecycle events Mounts

--- App.js
+++ App.js
@@ -1,17 +1,50 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = { val: 0 };
+		this.update = this.update.bind(this);
+	}
+	update() {
+		this.setState({ val: this.state.val + 1 })
+	}
+	componentWillMount() {
+		console.log("Mounting!")
+	}
 	render() {
-		return <Button>I <Heart /> React</Button>
+		console.log("Rendering!")
+		return <button onClick={this.update}>{this.state.val}</button>
+	}
+	componentDidMount() {
+		console.log("Mounted!")
+	}
+	componentWillUnmount() {
+		console.log("Unmount!")
 	}
 }
 
-class Button extends React.Component {
-	render () {
-		return <button>{this.props.children}</button>
+class Wrapper extends React.Component {
+	constructor() {
+		super();
+		this.mount = this.mount.bind(this);
+		this.unmount = this.unmount.bind(this);
+	}
+	mount() {
+		ReactDOM.render(<App />, document.getElementById('a'))
+	}
+	unmount() {
+		ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+	}
+	render() {
+		return (
+			<div>
+				<button onClick={this.mount}>Mount</button>
+				<button onClick={this.unmount}>Unmount</button>
+				<div id="a"></div>
+			</div>
+		)
 	}
 }
 
-const Heart = () => <span>♥</span>
-
-export default App
+export default Wrapper
