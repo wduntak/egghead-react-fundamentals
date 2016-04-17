3e24eabbd8ef32b1f5c9bc28d710653763992c2e
wduntak <wduntak@gmail.com>
2016-04-17 11:40:04 -0400

    Add a stateless function component

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+
 class App extends React.Component {
 	render() {
 		return (
-			<div>Hello!</div>
+			<div>
+				<h1>Hello World!</h1>
+				<b>Bold</b>
+			</div>
 		)
 	}
 }
+
+// Stateless Function Component
+// The difference is that the above class component can have state, where as
+// the stateless function component will not have state.
+
+// const App = () => <h1>Hello World!</h1>
 
 export default App;
