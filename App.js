49ce32812845154a5d44414785b927221d0ef899
wduntak <wduntak@gmail.com>
2016-04-17 14:49:27 -0400

    Dynamically generate compomponents and use unique key for sibling components

--- App.js
+++ App.js
@@ -1,50 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-let Mixin = InnerComponent => class extends React.Component {
-	constructor() {
-		super();
-		this.update = this.update.bind(this);
-		this.state = { val: 0 }
-	}
-	update() {
-		this.setState({val: this.state.val + 1})
-	}
-	componentWillMount() {
-		console.log("Will Mount")
-	}
-	render() {
-		return <InnerComponent
-			update={this.update}
-			{...this.state}
-			{...this.props} />
-	}
-	componentDidMount() {
-		console.log("Mounted")
-	}	
-}
-
-const Button = (props) => <button onClick={props.update}> 
-															{props.text} - {props.val} 
-													</button>
-
-const Label = (props) => <button onMouseMove={props.update}> 
-															{props.text} - {props.val} 
-													</button>
-
-let ButtonMixed = Mixin(Button)
-let LabelMixed = Mixin(Label)
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {data: [
+			{id:1, name: "Simon Baily"}, {id: 9, name: "Thomas Burleson"},
+			{id:2, name: "Charles Xavier"}, {id: 10, name: "Billy Bob"},
+			{id:3, name: "Logan McDonald"}, {id: 11, name: "Cho Gath"},
+			{id:4, name: "Scott Summers"}, {id: 12, name: "Supper Time"},
+			{id:5, name: "Jeanne Grey"}, {id: 13, name: "Willy Wonky"},
+			{id:6, name: "Bishop Knight"}, {id: 14, name: "Sharon Rose"},
+			{id:7, name: "Storm Goose"}, {id: 15, name: "Filip Doe"},
+			{id:8, name: "Chocolate Thunder"}, {id: 16, name: "Ross Thompson"}
+		]}
+	}
 	render() {
+		let rows = this.state.data.map( person => {
+			return <PersonRow key={person.id} data={person} />
+		})
 		return (
-			<div>
-				<LabelMixed text = "Label" />
-				<ButtonMixed text = "Button" />
-			</div>
+			<table>
+				<tbody>{rows}</tbody>
+			</table>
 		)
 	}
+}
+
+const PersonRow = (props) => {
+	return (
+		<tr >
+			<td>{props.data.id}</td>
+			<td>{props.data.name}</td>
+		</tr>
+	)
 }
 
 export default App	
