83cca68e0d7fc1a84f4452ad8017ff4f83723425
wduntak <wduntak@gmail.com>
2016-04-17 11:58:39 -0400

    Managing React state

--- App.js
+++ App.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	//Initializes the state
+	constructor() {
+		//Gives us the context for this within the component
+		super();
+		this.state = { 
+			text: 'This is the state text',
+			category: 0
+		}
+	}
+	update(e) {
+		this.setState({text: e.target.value})
+	}
 	render() {
 		let text = this.props.text
 		return (
 			<div>
-				<h1>Hello World! {text}</h1>
+				<b>Hello World! {text}</b><br />
+				<input 
+					type="text"
+					onChange={this.update.bind(this)} /> 
+				<p>{this.state.text}</p>
 			</div>
 		)
 	}
